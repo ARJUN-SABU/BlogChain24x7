32437b888e18f841f58591c63f153c5c1e9da336
ARJUN SABU <arjunsabu@MacBook-Pro.local>
2021-04-26 20:26:03 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Copyright &copy; BlogChain Inc. 2021</p>
+    </div>
+  );
+}
+
+export default Footer;
